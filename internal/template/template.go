2c919854822ed930aee839496d927b152656b7ee
Julien Breux <julien.breux@gmail.com>
2019-08-05 11:47:36 +0200

    template(write): Reduce desting too deep

--- internal/template/template.go
+++ internal/template/template.go
@@ -94,10 +94,19 @@ func (t *template) computeContent(img config.Image) ([]byte, error) {
 // Write writes the files
 func (t *template) Write() (err error) {
 	for _, s := range []files.State{files.StateCreated, files.StateChanged} {
-		for _, f := range t.files.List(s) {
-			if _, err := file.Write(f.Path(), f.Content()); err != nil {
-				return err
-			}
+		if err := t.writeFilesByState(s); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// writeFilesByStates writes all the files by state
+func (t *template) writeFilesByState(s files.State) error {
+	for _, f := range t.files.List(s) {
+		if _, err := file.Write(f.Path(), f.Content()); err != nil {
+			return err
 		}
 	}
 
