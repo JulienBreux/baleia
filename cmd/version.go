78bfed057b9c095b2654270f27a48b3e9d199472
Julien Breux <julien.breux@gmail.com>
2019-08-05 14:36:10 +0200

    cmd(version): Add author and website

--- cmd/version.go
+++ cmd/version.go
@@ -8,6 +8,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	app     = "Baleia"
+	author  = "Julien BREUX <julien.breux@gmail.com>"
+	website = "https://julienbreux.github.io/baleia/"
+)
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
@@ -18,9 +24,14 @@ var versionCmd = &cobra.Command{
 
 // versionRun represents the run command
 func versionRun(cmd *cobra.Command, args []string) {
+	fmt.Println(aurora.White(app))
+	fmt.Println()
 	fmt.Println(aurora.Cyan("Version:  "), aurora.White(version))
 	fmt.Println(aurora.Cyan("Commit:   "), aurora.White(commit))
 	fmt.Println(aurora.Cyan("Date:     "), aurora.White(date))
+	fmt.Println(aurora.Cyan("Author:   "), aurora.White(author))
+	fmt.Println()
+	fmt.Println(aurora.Cyan("Website:  "), aurora.White(website))
 
 	os.Exit(0)
 }
