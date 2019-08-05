0512684315fd9fdd90b22eeb8e1f503979655207
Julien Breux <julien.breux@gmail.com>
2019-08-05 11:58:21 +0200

    cmd(generate): Reduce command complexity

--- cmd/generate.go
+++ cmd/generate.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/julienbreux/baleia/internal/config"
@@ -15,70 +16,67 @@ const (
 )
 
 // generateCmd represents the generate command
-var generateCmd = &cobra.Command{
-	Use:   "generate",
-	Short: "Generate command is used to generate files",
-	Long: `Generate command is used to generate files.
+var (
+	generateCmd = &cobra.Command{
+		Use:   "generate",
+		Short: "Generate command is used to generate files",
+		Long: `Generate command is used to generate files.
 
 It's recommended to user the --dry option to display your changes.`,
-	Run: generateRun,
-}
+		Run: generateRun,
+	}
+
+	debug = false
+	file  = defaultFile
+	diff  = false
+	dry   = false
+)
 
 // generateRun represents the run command
 func generateRun(cmd *cobra.Command, args []string) {
-	debug, _ := cmd.Flags().GetBool("debug")
-	f, _ := cmd.Flags().GetString("file")
-	diff, _ := cmd.Flags().GetBool("diff")
-
-	c, err := config.New(f)
+	c, err := config.New(file)
 	if err != nil {
-		fmt.Println(aurora.Red(fmt.Sprintf("Unable to read config file '%s'", f)))
-		os.Exit(1)
+		printError(os.Stderr, fmt.Sprintf("Unable to read config file '%s'", file), nil)
 	}
-
-	// if err := c.Validate(); err != nil {
-	// 	fmt.Println(aurora.Red(fmt.Sprintf("Config is not valid '%s'", err)))
-	// 	os.Exit(1)
-	// }
 
 	// Open template
 	t, err := template.New(c)
 	if err != nil {
-		fmt.Println(aurora.Red(err))
-		os.Exit(1)
+		printError(os.Stderr, err.Error(), nil)
 	}
 
 	// Parse templates
 	if err := t.Parse(); err != nil {
-		fmt.Println(aurora.Red(fmt.Sprintf("Unable to parse template '%s'", c.GetTemplate())))
-		if err != nil && debug {
-			fmt.Println(aurora.Red(fmt.Sprintf(" ▹ %+v", err)))
-		}
-		os.Exit(1)
+		printError(os.Stderr, fmt.Sprintf("Unable to parse template '%s'", c.GetTemplate()), err)
 	}
 
 	// Print output
 	t.Print(os.Stdout, diff)
 
-	// Exit if only dry
-	if d, err := cmd.Flags().GetBool("dry"); d && err == nil {
-		os.Exit(0)
-	}
-
 	// Write templates changed
-	if err = t.Write(); err != nil {
-		fmt.Println(aurora.Red(fmt.Sprint("Unable to write files")))
-		os.Exit(1)
+	if !dry {
+		if err = t.Write(); err != nil {
+			printError(os.Stdout, "Unable to write files", nil)
+		}
 	}
 
 	os.Exit(0)
 }
 
+// printError prints an error O_o
+func printError(w io.Writer, str string, err error) {
+	fmt.Fprintln(w, aurora.Red(str))
+	if err != nil && debug {
+		fmt.Fprintln(w, aurora.Red(fmt.Sprintf(" ▹ %+v", err)))
+	}
+	os.Exit(1)
+}
+
 func init() {
 	rootCmd.AddCommand(generateCmd)
 
-	generateCmd.Flags().StringP("file", "f", defaultFile, "File used to work")
-	generateCmd.Flags().BoolP("dry", "y", false, "Dry mode to display changes only")
-	generateCmd.Flags().BoolP("diff", "i", false, "Display diff")
-	generateCmd.Flags().BoolP("debug", "d", false, "Debug mode")
+	generateCmd.Flags().StringVar(&file, "f", defaultFile, "File used to work")
+	generateCmd.Flags().BoolVar(&dry, "y", false, "Dry mode to display changes only")
+	generateCmd.Flags().BoolVar(&diff, "i", false, "Display diff")
+	generateCmd.Flags().BoolVar(&debug, "d", false, "Debug mode")
 }
